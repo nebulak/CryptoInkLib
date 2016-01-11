b7b8c1f133563a2385dddb9fce82a9c92458766c
nebulak <crypto-ink@gmx.de>
2016-01-11 21:28:09 +0100

    Fixed parts of Xmpp-OTR and added Logger

---
+++ src/Logger/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CryptoInkLib
+{
+	public class Logger
+	{
+		public static int LVL_TRACE = 0;
+		public static int LVL_DEBUG = 1;
+		public static int LVL_INFO = 2;
+		public static int LVL_WARNING = 3;
+		public static int LVL_ERROR = 4;
+
+		public Logger ()
+		{
+			m_sLogType = "memory";
+		}
+
+		public string m_sLogType;
+		public int m_iLogLevel;
+		public List<string> m_LogList
+
+
+		public void log(int iLogLevel, string sMessage, string sModule)
+		{
+			//check if LogLevel is high enough to be logged
+			if (iLogLevel < m_iLogLevel) {
+				return;
+			}
+
+
+
+		}
+	}
+}
+
