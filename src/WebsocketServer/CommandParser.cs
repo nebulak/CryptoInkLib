98557ed0be91ddd246691c8aedd7870c17b2310f
nebulak <nebulak@github.com>
2016-02-24 13:43:04 +0100

    added some basic websocket stuff

---
+++ src/WebsocketServer/CommandParser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace CryptoInkLib
+{
+	public class CommandParser
+	{
+		public CommandParser ()
+		{
+			
+		}
+
+
+		public string parseRequest(string sRequest)
+		{
+			WebsocketRequest request = JsonConvert.DeserializeObject<WebsocketRequest>(sRequest);
+
+			switch (request.command) 
+			{
+			case "getContacts":
+				this.getContacts ();
+			case "getConversations":
+				this.getConversations ();									
+			case "getNotifications":
+				this.getNotifications ();
+			case "getMessengerStatus":
+				this.getMessengerStatus ();
+			case "sendMessage":
+				this.sendMessage (request.parameters);
+			}
+
+				
+		}
+
+
+		public string getContacts()
+		{
+
+		}
+
+		public string getConversations()
+		{
+
+		}
+
+		public string getNotifications()
+		{
+
+		}
+
+		public string getMessengerStatus()
+		{
+
+		}
+
+		public string sendMessage(Dictionary<string, string> dParameters)
+		{
+			if (dParameters.ContainsKey ("receiver") && dParameters.ContainsKey ("message")) {
+				//TODO: implement send message
+			}
+		}
+	}
+}
+
---
+++ src/WebsocketServer/WebsocketRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptoInkLib
+{
+	public class WebsocketRequest
+	{
+		public WebsocketRequest ()
+		{
+		}
+
+		public string command;
+		public Dictionary<string, string> parameters;
+	}
+}
+
