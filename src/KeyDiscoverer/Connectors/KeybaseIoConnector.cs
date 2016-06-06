daafc74cbc95bc48d39c90ff28297b96c50a973e
nebulak <nebulak@github.com>
2016-06-06 19:20:16 +0200

    unstable version

---
+++ src/KeyDiscoverer/Connectors/KeybaseIoConnector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CryptoInkLib
+{
+	public class KeybaseIoConnector
+	{
+		public KeybaseIoConnector ()
+		{
+		}
+
+		public int getKeysForId(string sEmailAddress)
+		{
+			//TODO: get Info about key from Keybase.io
+
+		}
+
+		public int addKeyToKeyManager(string sEmailAddress)
+		{
+
+		}
+	}
+}
+
---
+++ src/KeyDiscoverer/KeyDiscoverer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CryptoInkLib
+{
+	public class dKeybaseApi
+	{
+		public dKeybaseApi ()
+		{
+		}
+
+
+		public string getProfile(string sID)
+		{
+			//TODO: implement function
+			return "";
+		}
+
+
+	}
+}
+
